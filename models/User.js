79d5af1aca1a65dad160e7a31d4c45500312fbc7
lamecksilva <lameck2@hotmail.com>
2018-09-16 16:29:09 -0300

    Creating new schema model of users

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Export the schema of users, and the actual schema
+module.exports = User = mongoose.model('users', UserSchema);
