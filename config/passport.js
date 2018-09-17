4dd81b07656102c00907cd317f44ecf35ba4ebde
lamecksilva <lameck2@hotmail.com>
2018-09-17 08:32:16 -0300

    Adding comments for the passport-jwt config file

--- config/passport.js
+++ config/passport.js
@@ -4,19 +4,19 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 const keys = require('../config/keys');
 
-const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
+const opts = {};      // Object containing the options to control how the token is extracted from request
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();     // Create a new extractor with the header bearer
+opts.secretOrKey = keys.secretOrKey;                        // Pass a public key for verifying token's signature
 
 module.exports = (passport) => {
   passport.use(new JwtStrategy(opts, (jwt_payload, done)=> {
-    User.findById(jwt_payload.id)
+    User.findById(jwt_payload.id)   // Search for the user with the id in the payload
       .then(user => {
         if(user){
-          return done(null,user);
+          return done(null,user);     // passport error callback accepting arguments done(error,user,info)
         }
         return done(null, false);
       })
-      .catch(err => console.log(err));
+      .catch(err => console.log(err));    // catch err
   }));
 };
