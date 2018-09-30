ba4d118a09d0863718e60bc959222e1f8b8e4f3b
lamecksilva <lameck2@hotmail.com>
2018-09-30 18:26:39 -0300

    Creating 1/2 store

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from "./components/layout/Navbar.js";
 import Footer from "./components/layout/Footer.js";
@@ -10,9 +12,11 @@ import Login from "./components/auth/Login.js";
 
 import "./App.css";
 
+
 class App extends Component {
   render() {
     return (
+      <Provider store={ store }>
       <Router>
         <div className="App">
           <Navbar />
@@ -24,6 +28,7 @@ class App extends Component {
           <Footer />
         </div>
       </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+
+
+const middleware = [thunk];
+
+const store = createStore(() => [], {}, applyMiddleware(...middleware));
+
+export default store;
