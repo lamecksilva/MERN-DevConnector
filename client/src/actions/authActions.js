bb2194e0baf38abfd43ee54a07758a8b404dccad
lamecksilva <lameck2@hotmail.com>
2018-10-05 08:56:46 -0300

    Add register and redirect with redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,18 @@
-import { TEST_DISPATCH } from "./types";
+import { GET_ERRORS } from "./types";
+import axios from "axios";
+
 // Register User
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+
+// The registerUser receive data in params
+// And return the type of action, with the payload: userData
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
