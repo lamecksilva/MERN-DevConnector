0e5eb58bf44fca9a36878b88564c13962b874ffd
lamecksilva <lameck2@hotmail.com>
2018-10-05 09:46:36 -0300

    Adding redux to save the token in the current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from "../utils/setAuthToken";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 // Register User
 
@@ -16,3 +18,36 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+// Login - Get User Token
+export const loginuser = userData => dispatch => {
+  axios
+    .post("/api/user/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      // Set token to auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded))
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+
+// Set logged in user
+export const setCurrentUser = (decode) => {
+  return{
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
