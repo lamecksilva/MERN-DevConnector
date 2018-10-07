daa4fd47dd95a00cde7d3c1fb2afe01f41c81a61
lamecksilva <lameck2@hotmail.com>
2018-10-07 17:57:51 -0300

    Adicionando 'privateRoute' para quando o usuario der logout, redirecionar para a rota login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
@@ -7,12 +7,14 @@ import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
+import PrivateRoute from "./components/common/PrivateRoute";
+
 import Navbar from "./components/layout/Navbar.js";
 import Footer from "./components/layout/Footer.js";
 import Landing from "./components/layout/Landing.js";
 import Register from "./components/auth/Register.js";
 import Login from "./components/auth/Login.js";
-import Dashboard from './components/dashboard/Dashboard';
+import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
@@ -50,7 +52,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer />
           </div>
