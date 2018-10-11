333e877b4d5b72d0a220aef19be17439baaaa4f2
lamecksilva <lameck2@hotmail.com>
2018-10-10 23:17:52 -0300

    Adicionando pagina de editar o perfil e os campos são checados e preenchidos automaticamente, para o usuário editar os campos

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import CreateProfile from "./components/create-profile/CreateProfile";
 
 import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
+import EditProfile from "./components/edit-profile/EditProfile";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -62,6 +63,13 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
                 />
               </Switch>
             </div>
