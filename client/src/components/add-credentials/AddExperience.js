6fc25e41786a78af2c2f3cc5737a11b66522b842
lamecksilva <lameck2@hotmail.com>
2018-10-11 09:36:42 -0300

    Parei no meio da aula 56

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
-import { TextFieldGroup } from "../common/TextFieldGroup";
+import TextFieldGroup from "../common/TextFieldGroup";
 import { TextAreaFieldGroup } from "../common/TextAreaFieldGroup";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
@@ -34,12 +34,40 @@ class AddExperience extends Component {
               <Link to="/dashboard" className="btn btn-light">
                 Go Back
               </Link>
-              <h1 className="display-4 text-center">Add Experiente</h1>
+              <h1 className="display-4 text-center">Add Experience</h1>
               <p className="lead-center">
                 Add any job or position that you have had in the past or current
               </p>
               <small className="d-block pb-3">* = required Fields</small>
-              
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Company"
+                  name="company"
+                  value={this.state.company}
+                  onChange={this.onChange}
+                  error={errors.company}
+                />
+
+                <TextFieldGroup
+                  placeholder="* Job Title"
+                  name="title"
+                  value={this.state.title}
+                  onChange={this.onChange}
+                  error={errors.title}
+                />
+
+                <TextFieldGroup
+                  placeholder="Location"
+                  name="location"
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                />
+
+                <h6>From Date</h6>
+                
+                
+              </form>
             </div>
           </div>
         </div>
