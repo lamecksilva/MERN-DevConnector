86fb8d2c44013c100ea412207c89aaa56bdff498
lamecksilva <lameck2@hotmail.com>
2018-10-12 17:32:52 -0300

    Adicionando componnente para criação de novo post e interação com o back end e com o banco de dados

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from './components/posts/Posts'
 
 
 import "./App.css";
@@ -92,6 +93,13 @@ class App extends Component {
                   exact
                   path="/add-education"
                   component={AddEducation}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/feed"
+                  component={Posts}
                 />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,20 @@
+import { ADD_POST } from '../actions/types';
+
+
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = initialState, action){
+  switch(action.type){
+    case ADD_POST:
+     return {
+       ...state,
+       posts: [action.payload, ...state.posts]
+     }
+    default:
+      return state;
+  }
+}
