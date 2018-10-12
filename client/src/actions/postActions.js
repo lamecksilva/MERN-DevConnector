f0c0a471e96108891c0054206f51a67fb68c04d0
lamecksilva <lameck2@hotmail.com>
2018-10-12 20:08:12 -0300

    Sueli maria

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS } from "./types";
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -20,3 +20,30 @@ export const addPost = postData => dispatch => {
     );
 };
 
+// Add Post
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading);
+
+  axios
+    .get("api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Set Loading state
+
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
+};
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,5 +1,4 @@
-import { ADD_POST } from '../actions/types';
-
+import { ADD_POST, GET_POSTS, POST_LOADING } from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -7,13 +6,25 @@ const initialState = {
   loading: false
 };
 
-export default function(state = initialState, action){
-  switch(action.type){
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
     case ADD_POST:
-     return {
-       ...state,
-       posts: [action.payload, ...state.posts]
-     }
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
     default:
       return state;
   }
