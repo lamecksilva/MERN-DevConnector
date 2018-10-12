9909275505f12e6cc4bcafb60761a6b4cd1b1dc7
lamecksilva <lameck2@hotmail.com>
2018-10-12 11:28:24 -0300

    Adicionando os perfis na url que mostra todos os perfis da rede social

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import spinner from '../common/Spinner';
-import { getProfiles } from '../../actions/profileActions';
-import Spinner from '../common/Spinner';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Spinner from "../common/Spinner";
+import ProfileItem from "./ProfileItem";
+import { getProfiles } from "../../actions/profileActions";
 
 class Profiles extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.getProfiles();
   }
 
@@ -15,13 +14,15 @@ class Profiles extends Component {
     const { profiles, loading } = this.props.profile;
     let profileItems;
 
-    if(profiles === null || loading){
-      profileItems = <Spinner />
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
     } else {
-      if(profiles.length > 0){
-        <h1>PROFILES HERE</h1>
+      if (profiles.length > 0) {
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ));
       } else {
-        profileItems = <h4>No Profiles Found...</h4>
+        profileItems = <h4>No Profiles Found...</h4>;
       }
     }
 
@@ -32,24 +33,27 @@ class Profiles extends Component {
             <div className="col-md-12">
               <h1 className="display-4 text-center">Developer profiles</h1>
               <p className="lead text-center">
-              Browse and connect with developers
+                Browse and connect with developers
               </p>
               {profileItems}
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 Profiles.propTypes = {
   getProfiles: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps = state =>({
+const mapStateToProps = state => ({
   profile: state.profile
-})
+});
 
-export default connect(mapStateToProps, {getProfiles})(Profiles);
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
